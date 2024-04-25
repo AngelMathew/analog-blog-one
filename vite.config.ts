5e622870869635adc1fd1483349dd845b0ad0c61
angelmathew <angelannamathew07@gmail.com>
2024-04-25 18:10:27 -0400

    render using ssr

--- vite.config.ts
+++ vite.config.ts
@@ -12,33 +12,11 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  // prerender property to configure routes to be rendered at build time.
   plugins: [
     analog({
-      static: true,
-      prerender: {
-        routes: async () => [
-          '/',
-          '/about',
-          '/blog',
-          {
-            contentDir: 'src/content/blog',
-            transform: (file: PrerenderContentFile) => {
-              // do not include files marked as draft in frontmatter
-              if (file.attributes?.['draft']) {
-                return false;
-              }
-              // use the slug from frontmatter if defined, otherwise use the files basename
-              const slug = file.attributes?.['slug'] || file.name;
-              return `/blog/${slug}`;
-            },
-
-          },
-        ],
-        sitemap: {
-          host: ' https://662a72e15de2a603cf015a9a--comforting-taiyaki-efdb1f.netlify.app/blog',
-        },
-      },
+      // prerender: {
+      //   routes: ['/', '/about','/blog'],
+      // },
     }),
   ],
   test: {
